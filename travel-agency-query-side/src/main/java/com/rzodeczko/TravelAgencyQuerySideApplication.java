ceba916b4155c83d9c038ca08e54ce46f63e4177
CoderNoOne <firelight.code@gmail.com>
2026-06-07 17:04:42 +0200

    Add initial project structure with configuration files and Avro schemas

---
+++ travel-agency-query-side/src/main/java/com/rzodeczko/TravelAgencyQuerySideApplication.java
@@ -0,0 +1,13 @@
+package com.rzodeczko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyQuerySideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyQuerySideApplication.class, args);
+    }
+
+}
