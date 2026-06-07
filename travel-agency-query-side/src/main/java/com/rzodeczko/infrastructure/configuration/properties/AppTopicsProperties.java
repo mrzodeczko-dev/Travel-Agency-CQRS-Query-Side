0ffd4518fae32ea7aa3a7c43a5a0f22bcd5f5a2b
CoderNoOne <firelight.code@gmail.com>
2026-06-07 18:59:43 +0200

    Fix typo in AppTopicsProperties and add PropertiesConfiguration class for property binding

--- travel-agency-query-side/src/main/java/com/rzodeczko/infrastructure/configuration/properties/AppTopicsProperties.java
+++ travel-agency-query-side/src/main/java/com/rzodeczko/infrastructure/configuration/properties/AppTopicsProperties.java
@@ -1,12 +1,10 @@
 package com.rzodeczko.infrastructure.configuration.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(AppTopicsProperties.class)
 @ConfigurationProperties(prefix = "app.topics")
 public record AppTopicsProperties(
-        String bookinsgs,
+        String bookings,
         String dailyAvailability,
         String dailyAvailabilityDlt,
         String hotels,
---
+++ travel-agency-query-side/src/main/java/com/rzodeczko/infrastructure/configuration/properties/PropertiesConfiguration.java
@@ -0,0 +1,7 @@
+package com.rzodeczko.infrastructure.configuration.properties;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(AppTopicsProperties.class)
+public class PropertiesConfiguration {
+}
